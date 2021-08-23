ed2f226a6abdd2e234727191683da716a8aeb21c
Hotae Kim <hotae.kim.94@gmail.com>
2021-08-23 18:56:38 -0400

    first commit with hash table algorithm

---
+++ hash/linear_probing_hash.js
@@ -0,0 +1,108 @@
+class HashTable {
+  constructor(size = 1000) {
+    this.arr = new Array(size);
+  }
+
+  hash(s) {
+    let hash = 0;
+    for (let c of s) {
+      let code = c.charCodeAt();
+      hash = (Math.abs(hash << 5) - hash) + code;
+      hash = hash & hash; // Convert to 32bit integer
+    }
+    return hash;
+  }
+
+  keyToIndex(key) {
+    const stringKey = key.toString()
+    return this.hash(stringKey) % this.arr.length;
+  }
+
+  has(key) {
+    const initialIndex = this.keyToIndex(key);
+    if (this.arr[initialIndex] == null) return -1;
+    if (this.arr[initialIndex][0] === key.toString()) return initialIndex;
+    let runningIndex = initialIndex + 1;
+    while (
+      this.arr[runningIndex] != null &&
+      this.arr[runningIndex][0] !== key.toString() &&
+      runningIndex !== initialIndex
+    ) {
+      runningIndex = (runningIndex + 1) % this.arr.length;
+    }
+    if (runningIndex === initialIndex) return -1;
+    return this.arr[runningIndex] == null ? -1 : runningIndex;
+  }
+
+  set(key, value) {
+    const hasIndex = this.has(key);
+    if (hasIndex !== -1) {
+      this.arr[hasIndex][1] = value;
+    } else {
+      const initialIndex = this.keyToIndex(key);
+      let runningIndex = initialIndex;
+      let bool = false;
+      while (!bool) {
+        if (!this.arr[runningIndex] || this.arr[runningIndex] === "deleted") {
+          this.arr[runningIndex] = [key.toString(), value];
+          bool = true;
+        }
+       runningIndex = (runningIndex + 1) % this.arr.length;
+       if (runningIndex === initialIndex) {
+         console.log("Table is full");
+         bool = true;
+       }
+      }
+    }
+  }
+
+  get(key) {
+    const index = this.has(key);
+    return index !== -1 ? this.arr[index][1] : "not found";
+  }
+
+  delete(key) {
+    const index = this.has(key);
+    if (index !== -1) this.arr[index] = "deleted";
+  }
+}
+
+let tmp = 0;
+let tmp2 = 0;
+
+const ht = new HashTable();
+for (let i = 0; i < 1000; i++) {
+  ht.set(i, i);
+  tmp += i;
+}
+
+for (let i of ht.arr) {
+  if (i && typeof i === "object") tmp2 += i[1];
+}
+
+console.log(tmp, tmp2)
+
+for (let i = 0; i < 1000; i++) {
+  if (i % 2 === 0) ht.delete(i);
+}
+
+let deletedCount = 0;
+
+for (let a of ht.arr) {
+  if (a === "deleted") deletedCount++;
+}
+
+console.log(deletedCount);
+
+let getCount = 0;
+
+for (let i = 1; i < 1000; i+=2) {
+  let value = ht.get(i);
+  if (value === "not found") throw new Error("fix your code");
+}
+
+for (let i = 0; i < 1000; i+=2) {
+  let value = ht.get(i);
+  if (value !== "not found") throw new Error("fuk you");
+}
+
---
+++ hash/linked_list_hash.js
@@ -0,0 +1,109 @@
+class Node {
+  constructor(key, value) {
+    this.key = key;
+    this.value = value;
+    this.prev = null;
+    this.next = null;
+  }
+}
+
+class HashTable {
+  constructor(size = 1) {
+    this.arr = new Array(size);
+  }
+
+  hash(s) {
+    let hash = 0;
+    for (let c of s) {
+      let code = c.charCodeAt();
+      hash = (Math.abs(hash << 5) - hash) + code;
+      hash = hash & hash; // Convert to 32bit integer
+    }
+    return hash;
+  }
+
+  keyToIndex(key) {
+    const stringKey = key.toString();
+    return this.hash(stringKey) % this.arr.length;
+  }
+
+  set(key, value) {
+    const index = this.keyToIndex(key);
+    if (!this.arr[index]) this.arr[index] = new Node(key, value);
+    if (this.arr[index]) {
+      let curr = this.arr[index];
+      while (true) {
+        if (curr.key === key) {
+          curr.value = value;
+          return
+        }
+        if (!curr.next) {
+          curr.next = new Node(key, value);
+          curr.next.prev = curr;
+          return
+        }
+        curr = curr.next;
+      }
+    }
+  }
+
+  get(key) {
+    const index = this.keyToIndex(key);
+    let curr = this.arr[index];
+    while (true) {
+      if (curr.key === key) return curr.value;
+      if (!curr.next) return "not found";
+      curr = curr.next;
+    }
+  }
+
+  delete(key) {
+    const index = this.keyToIndex(key);
+    let curr = this.arr[index];
+    while (true) {
+      if (curr.key === key) {
+        //if curr is the head
+        if (!curr.prev) {
+          //if head has next
+          if (!curr.next) {
+            this.arr[index] = null;
+            return
+          } else {
+            this.arr[index] = curr.next;
+            return
+          }
+        //curr is not the head
+        } else {
+          //curr doesn't have next -- no need to connect between prev and next
+          if (!curr.next) {
+            curr.prev.next = null;
+            return
+          } else {
+            //connect curr's next to curr's prev
+            let newNext = curr.next;
+            newNext.prev = curr.prev;
+            curr.prev.next = newNext;
+            return
+          }
+        }
+      }
+      if (curr.next) {
+        curr = curr.next;
+      } else {
+        return
+      }
+    }
+  }
+}
+
+const ht = new HashTable();
+ht.set("JunMo", "Korea");
+ht.set("Kevin", "Canada");
+ht.set("Justin", "Canada");
+console.log(ht.get("Kevin"));
+console.log(ht.get("Justin"));
+ht.delete("Kevin");
+console.log(ht.get("Kevin"));
+console.log(ht.get("Justin"));
+ht.delete("SiDragon");
+console.log(ht.arr[0]);
